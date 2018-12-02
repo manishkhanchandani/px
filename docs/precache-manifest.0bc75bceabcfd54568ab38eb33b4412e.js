5fa66c365203d4aa1a94c5bd0bc098fef881670b
Manish Khanchandani <manishkk74@gmail.com>
2018-12-02 23:46:27 +0530

    untracked file

---
+++ docs/precache-manifest.0bc75bceabcfd54568ab38eb33b4412e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a35558fc64f0ec79b9a",
+    "url": "/px/static/js/runtime~main.1a35558f.js"
+  },
+  {
+    "revision": "28106a917315914787e4",
+    "url": "/px/static/js/main.28106a91.chunk.js"
+  },
+  {
+    "revision": "5a6e8d7c6684b350ad98",
+    "url": "/px/static/js/5.5a6e8d7c.chunk.js"
+  },
+  {
+    "revision": "09325b47292aa98b78f6",
+    "url": "/px/static/js/4.09325b47.chunk.js"
+  },
+  {
+    "revision": "f5aabf9a8726cb1f69f7",
+    "url": "/px/static/js/3.f5aabf9a.chunk.js"
+  },
+  {
+    "revision": "68c467478ff4acfdd981",
+    "url": "/px/static/js/2.68c46747.chunk.js"
+  },
+  {
+    "revision": "3cf53ae00a07cd5306b1",
+    "url": "/px/static/js/0.3cf53ae0.chunk.js"
+  },
+  {
+    "revision": "28106a917315914787e4",
+    "url": "/px/static/css/main.d0b25db4.chunk.css"
+  },
+  {
+    "revision": "9317e9c14f4604928ed3a1bf7703929b",
+    "url": "/px/index.html"
+  }
+];
